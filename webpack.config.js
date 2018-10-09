7c6d5daadb8fa8905224c38deef31a9bf7c35450
sineau <sineaugh@gmail.com>
2018-10-09 14:31:43 +0330

    Add extension base setup

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const HtmlPlugin = require('html-webpack-plugin');
 const host = "localhost";
 const port = 8080;
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+const IS_EXT = process.env.NODE_ENV === 'ext';
 const dist = IS_PRODUCTION ? 'prod' : 'dev';
 
 const DIST_DIR = path.resolve(__dirname, dist)
@@ -16,18 +17,21 @@ const ASSET_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif', 'eot', 'otf', 'svg', 'ttf
 module.exports = {
   context: SRC_DIR,
   entry: {
-    popup: './popup.jsx'
+    popup: './popup',
+    highlight: './highlight',
+    background: './background',
+    app: './app/app'
   },
   output: {
     filename: '[name].bundle.js',
-    publicPath: `/${dist}/`
+    path: path.join(__dirname, `./${dist}`)
   },
   devServer: {
     host: host,
     port: port,
     overlay: true,
     publicPath: '/dev/',
-//    contentBase: DIST_DIR
+    //    contentBase: DIST_DIR
   },
   resolve: {
     extensions: ['.js','.jsx']
@@ -37,22 +41,37 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        use: 'babel-loader',
+        use: [
+          'babel-loader',
+          'eslint-loader'
+        ]
       },
+      {
+        test: require.resolve('webextension-polyfill'),
+        use: 'imports-loader?browser=>undefined'
+      }
     ]
   },
   plugins: [
     new HtmlPlugin({
-      template: 'template.html'
+      template: 'template.html',
+      chunks: ['popup']
     }),
-//    new WriteFilePlugin({}),
-    new webpack.ProvidePlugin({
+    new HtmlPlugin({
+      template: 'template.html',
+      chunks: ['app'],
+      title: 'Research Assistant',
+      filename: 'app.html'
+    }),
+
+    new WriteFilePlugin(),
+    IS_EXT ? new webpack.ProvidePlugin({
       browser: 'webextension-polyfill'
-    }),
-    new WebpackShellPlugin({
+    }) : new Function(),
+    IS_EXT ? new WebpackShellPlugin({
       onBuildStart:[`web-ext run --s ${dist}`]
-    }),
+    }) : new Function(),
     IS_PRODUCTION ? new MinifyPlugin() : /* no-op */ new Function()
   ],
-  devtool: IS_PRODUCTION ? '' : 'inline-source-map'
+  devtool: IS_PRODUCTION ? '' : 'cheap-module-source-map'
 }
